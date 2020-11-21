a75ba1a76ba90ba75deb7f0b8ddbcb922e6db4d7
Daniel Sandu <49592096+dansandu@users.noreply.github.com>
2020-11-22 01:02:43 +0200

    Add README example test

--- sources/dansandu/jelly/json.test.cpp
+++ sources/dansandu/jelly/json.test.cpp
@@ -340,4 +340,54 @@ TEST_CASE("Json")
         REQUIRE_THROWS_AS(Json::deserialize(R"({"duplicateKey": false, "duplicateKey": "value"})"),
                           JsonDeserializationError);
     }
+
+    SECTION("README example")
+    {
+        auto order = Json::deserialize(R"({
+            "orderId": "471fc736-56e9-4a78-a256-4b6f641b7d13",
+            "items": [
+                {
+                    "itemId": "e01d9d5d-2994-4eeb-b24d-0c80fcfe7be0",
+                    "count": 5
+                },
+                {
+                    "itemId": "52cace0f-e482-4cf0-a5e5-0d8728e3015d",
+                    "count": 2
+                }
+            ],
+            "hasPromotion": true,
+            "vat": 0.20,
+            "previousOrderId": null
+        })");
+
+        const std::string orderId = order["orderId"];
+        const int firstItemCount = order["items"][0]["count"];
+        const auto secondItemCount = order["items"][1]["count"].get<int>();
+        const double vat = order["vat"];
+        const bool hasPreviousOrder = !order["previousOrderId"].is<std::nullptr_t>();
+
+        // const std::nullptr_t    badCast1 = order["previousOrderId"]; // won't compile
+        // const int*              badCast2 = order["previousOrderId"];  // won't compile
+        // const unsigned          badCast3 = order["vat"];              // won't compile
+        // const int               badCast4 = order["vat"];              // compiles but throws exception
+        // const Json::list_type   badCast5 = order["items"];            // won't compile
+        // const Json::object_type badCast6 = order;                     // won't compile
+
+        auto total = (1.0 + vat) * (firstItemCount * 10.99 + secondItemCount * 55.99);
+
+        if (order["hasPromotion"])
+        {
+            total *= 0.90;
+        }
+
+        auto orderReceipt = Json::object();
+        orderReceipt["orderId"] = order["orderId"];
+        orderReceipt["total"] = total;
+
+        REQUIRE(!hasPreviousOrder);
+
+        REQUIRE(static_cast<std::string>(orderReceipt["orderId"]) == "471fc736-56e9-4a78-a256-4b6f641b7d13");
+
+        REQUIRE(static_cast<double>(orderReceipt["total"]) == 180.2844);
+    }
 }
