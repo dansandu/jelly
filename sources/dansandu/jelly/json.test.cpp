cbb2b4833486f0f378a5a5b0b23e102ff39f613e
Daniel Sandu <49592096+dansandu@users.noreply.github.com>
2021-06-02 07:24:39 +0300

    Clean up json test

--- sources/dansandu/jelly/json.test.cpp
+++ sources/dansandu/jelly/json.test.cpp
@@ -2,62 +2,64 @@
 #include "dansandu/jelly/error.hpp"
 #include "dansandu/jelly/json.hpp"
 
-#include <map>
-#include <string>
-#include <vector>
-
 using Catch::Detail::Approx;
 using dansandu::jelly::error::JsonDeserializationError;
 using dansandu::jelly::json::Json;
 
 TEST_CASE("Json")
 {
-    SECTION("primitives")
+    SECTION("primitive")
     {
         SECTION("null")
         {
-            auto jsonAsString = "null";
+            const auto string = "null";
 
-            auto json = Json::deserialize(jsonAsString);
+            const auto json = Json::deserialize(string);
 
-            REQUIRE(json.serialize() == jsonAsString);
+            REQUIRE(json.serialize() == string);
 
             REQUIRE(json.is<Json::null_type>());
+
+            REQUIRE_THROWS_AS(static_cast<Json::string_type>(json), std::logic_error);
         }
 
         SECTION("boolean")
         {
-            auto jsonAsString = "false";
+            const auto string = "true";
 
-            auto json = Json::deserialize(jsonAsString);
+            const auto json = Json::deserialize(string);
 
-            REQUIRE(json.serialize() == jsonAsString);
+            REQUIRE(json.serialize() == string);
 
             REQUIRE(json.is<bool>());
 
-            REQUIRE(!json.get<bool>());
+            REQUIRE(json.get<bool>());
+
+            REQUIRE_THROWS_AS(static_cast<int>(json), std::logic_error);
         }
 
         SECTION("integer")
         {
-            auto jsonAsString = "73";
+            const auto string = "73";
 
-            auto json = Json::deserialize(jsonAsString);
+            const auto json = Json::deserialize(string);
 
-            REQUIRE(json.serialize() == jsonAsString);
+            REQUIRE(json.serialize() == string);
 
             REQUIRE(json.is<int>());
 
             REQUIRE(json.get<int>() == 73);
+
+            REQUIRE_THROWS_AS(static_cast<double>(json), std::logic_error);
         }
 
         SECTION("double")
         {
-            auto jsonAsString = "0.125";
+            const auto string = "0.125";
 
-            auto json = Json::deserialize(jsonAsString);
+            const auto json = Json::deserialize(string);
 
-            REQUIRE(json.serialize() == jsonAsString);
+            REQUIRE(json.serialize() == string);
 
             REQUIRE(json.is<double>());
 
@@ -66,219 +68,135 @@ TEST_CASE("Json")
 
         SECTION("string")
         {
-            auto jsonAsString = R"("some_value")";
+            const auto string = R"("some_value")";
 
-            auto json = Json::deserialize(jsonAsString);
+            const auto json = Json::deserialize(string);
 
-            REQUIRE(json.serialize() == jsonAsString);
+            REQUIRE(json.serialize() == string);
 
-            REQUIRE(json.is<std::string>());
+            REQUIRE(json.is<Json::string_type>());
 
-            REQUIRE(json.get<std::string>() == "some_value");
+            REQUIRE(json.get<Json::string_type>() == "some_value");
         }
 
         SECTION("list")
         {
-            auto jsonAsString = "[79,2485,93]";
+            const auto string = R"([79,false,"str"])";
 
-            auto json = Json::deserialize(jsonAsString);
+            const auto json = Json::deserialize(string);
 
-            REQUIRE(json.serialize() == jsonAsString);
+            REQUIRE(json.serialize() == string);
 
             REQUIRE(json.is<Json::list_type>());
 
             REQUIRE(json[0].get<int>() == 79);
 
-            REQUIRE(json[1].get<int>() == 2485);
+            REQUIRE(!json[1].get<bool>());
 
-            REQUIRE(json[2].get<int>() == 93);
+            REQUIRE(json[2].get<Json::string_type>() == "str");
+
+            REQUIRE_THROWS_AS(json[3], std::logic_error);
         }
 
         SECTION("empty list")
         {
-            auto jsonAsString = "[]";
+            const auto string = "[]";
 
-            auto json = Json::deserialize(jsonAsString);
+            const auto json = Json::deserialize(string);
 
             REQUIRE(json.get<Json::list_type>().empty());
         }
 
         SECTION("object")
         {
-            auto jsonAsString = R"({"a":1,"b":2,"c":3})";
+            const auto string = R"({"a":null,"b":[-10,-0.5],"c":{"d":"value"}})";
 
-            auto json = Json::deserialize(jsonAsString);
+            const auto json = Json::deserialize(string);
 
-            REQUIRE(json.serialize() == jsonAsString);
+            REQUIRE(json.serialize() == string);
 
             REQUIRE(json.is<Json::object_type>());
 
-            REQUIRE(json["a"].get<int>() == 1);
+            REQUIRE(json["a"].is<Json::null_type>());
 
-            REQUIRE(json["b"].get<int>() == 2);
+            REQUIRE(json["b"][0].get<int>() == -10);
 
-            REQUIRE(json["c"].get<int>() == 3);
+            REQUIRE(json["b"][1].get<double>() == -0.5);
+
+            REQUIRE(json["c"]["d"].get<Json::string_type>() == "value");
+
+            REQUIRE_THROWS_AS(json["e"], std::logic_error);
         }
 
         SECTION("empty object")
         {
-            auto jsonAsString = "{}";
+            const auto string = "{}";
 
-            auto json = Json::deserialize(jsonAsString);
+            const auto json = Json::deserialize(string);
 
             REQUIRE(json.get<Json::object_type>().empty());
         }
     }
 
-    SECTION("medium json")
+    SECTION("mutate")
     {
-        auto jsonAsString =
-            R"({"array":[1,2,3],"boolean":true,"floatingPoint":0.25,"integer":7,"null":null,"string":"myString"})";
+        auto json = Json::deserialize(R"({"array":[0,1],"bool":true,"real":0.25})");
 
-        auto json = Json::deserialize(jsonAsString);
-
-        REQUIRE(json.serialize() == jsonAsString);
-
-        REQUIRE(json.is<Json::object_type>());
-
-        SECTION("retrieval with getters")
+        SECTION("null")
         {
-            auto map = json.get<Json::object_type>();
-
-            SECTION("throws on wrong value")
-            {
-                REQUIRE_THROWS_AS(json.get<std::string>(), std::logic_error);
-            }
-
-            SECTION("string value")
-            {
-                auto value = map.at("string");
-
-                REQUIRE(value.is<std::string>());
-
-                REQUIRE(value.get<std::string>() == "myString");
-            }
-
-            SECTION("integer value")
-            {
-                auto value = map.at("integer");
-
-                REQUIRE(value.is<int>());
-
-                REQUIRE(value.get<int>() == 7);
-            }
-
-            SECTION("floating point value")
-            {
-                auto value = map.at("floatingPoint");
-
-                REQUIRE(value.is<double>());
-
-                REQUIRE(value.get<double>() == 0.25);
-            }
-
-            SECTION("null value")
-            {
-                auto value = map.at("null");
-
-                REQUIRE(value.is<Json::null_type>());
-
-                REQUIRE(value.get<Json::null_type>() == nullptr);
-            }
-
-            SECTION("boolean value")
-            {
-                auto value = map.at("boolean");
-
-                REQUIRE(value.is<bool>());
-
-                REQUIRE(value.get<bool>());
-            }
-
-            SECTION("array value")
-            {
-                auto value = map.at("array");
-
-                REQUIRE(value.is<Json::list_type>());
-
-                auto vector = value.get<Json::list_type>();
-
-                REQUIRE(vector.size() == 3);
-
-                REQUIRE(vector[0].is<int>());
-
-                REQUIRE(vector[0].get<int>() == 1);
-
-                REQUIRE(vector[1].is<int>());
-
-                REQUIRE(vector[1].get<int>() == 2);
-
-                REQUIRE(vector[2].is<int>());
-
-                REQUIRE(vector[2].get<int>() == 3);
-            }
-        }
-
-        SECTION("sugar")
-        {
-            REQUIRE(json["string"].get<std::string>() == "myString");
-
-            REQUIRE(json["boolean"].get<bool>());
-
-            REQUIRE(json["floatingPoint"].get<double>() == Approx(0.25));
-
-            REQUIRE(json["integer"].get<int>() == 7);
+            json["null"];
 
             REQUIRE(json["null"].get<Json::null_type>() == nullptr);
-
-            REQUIRE(json["array"][0].get<int>() == 1);
-
-            REQUIRE(json["array"][1].get<int>() == 2);
-
-            REQUIRE(json["array"][2].get<int>() == 3);
-
-            REQUIRE_THROWS_AS(json[0], std::logic_error);
-
-            REQUIRE_THROWS_AS(json["array"]["first"], std::logic_error);
-
-            REQUIRE_THROWS_AS(static_cast<std::string>(json["boolean"]), std::logic_error);
         }
 
-        SECTION("add new object member")
+        SECTION("integer")
         {
-            SECTION("initialization")
-            {
-                json["newNull"];
+            json["int"] = 999;
 
-                REQUIRE(json["newNull"].get<Json::null_type>() == nullptr);
-            }
+            REQUIRE(json["int"].get<int>() == 999);
+        }
 
-            SECTION("assignment")
-            {
-                json["newInteger"] = 999;
+        SECTION("string")
+        {
+            json["str"] = Json::string_type{"abc"};
 
-                REQUIRE(json["newInteger"].get<int>() == 999);
-            }
+            REQUIRE(json["str"].get<Json::string_type>() == "abc");
+        }
+
+        SECTION("array")
+        {
+            json["array"] = Json::deserialize("[11, 12]");
+
+            REQUIRE(json["array"][0].get<int>() == 11);
+
+            REQUIRE(json["array"][1].get<int>() == 12);
+        }
+
+        SECTION("boolean")
+        {
+            json["bool"] = false;
+
+            REQUIRE(!json["bool"].get<bool>());
         }
     }
 
     SECTION("big json")
     {
-        auto jsonAsString = R"([{"battery":88,"identifier":"f2c4deb09cc1558","lastCharge":1597779221,"list":[],)"
+        const auto string = R"([{"battery":88,"identifier":"f2c4deb09cc1558","lastCharge":1597779221,"list":[],)"
                             R"("location":[50,10],"samples":{"CO":2,"O2":19},"timestamp":1597780427},)"
                             R"({"battery":41,"identifier":"1eb6731c7132367","lastCharge":null,"location":[26,32],)"
                             R"("map":{},"samples":{"CO":18,"O2":10},"timestamp":1597780001}])";
 
-        auto json = Json::deserialize(jsonAsString);
+        auto json = Json::deserialize(string);
 
         SECTION("serialized")
         {
-            REQUIRE(json.serialize() == jsonAsString);
+            REQUIRE(json.serialize() == string);
         }
 
         SECTION("check values")
         {
-            REQUIRE(json[0]["identifier"].get<std::string>() == "f2c4deb09cc1558");
+            REQUIRE(json[0]["identifier"].get<Json::string_type>() == "f2c4deb09cc1558");
 
             REQUIRE(json[0]["list"].get<Json::list_type>().empty());
 
@@ -296,7 +214,7 @@ TEST_CASE("Json")
 
             REQUIRE(json[0]["lastCharge"].get<int>() == 1597779221);
 
-            REQUIRE(json[1]["identifier"].get<std::string>() == "1eb6731c7132367");
+            REQUIRE(json[1]["identifier"].get<Json::string_type>() == "1eb6731c7132367");
 
             REQUIRE(json[1]["map"].get<Json::object_type>().empty());
 
@@ -343,7 +261,7 @@ TEST_CASE("Json")
 
     SECTION("README example")
     {
-        auto order = Json::deserialize(R"({
+        const auto order = Json::deserialize(R"({
             "orderId": "471fc736-56e9-4a78-a256-4b6f641b7d13",
             "items": [
                 {
@@ -366,13 +284,6 @@ TEST_CASE("Json")
         const double vat = order["vat"];
         const bool hasPreviousOrder = !order["previousOrderId"].is<std::nullptr_t>();
 
-        // const std::nullptr_t    badCast1 = order["previousOrderId"]; // won't compile
-        // const int*              badCast2 = order["previousOrderId"];  // won't compile
-        // const unsigned          badCast3 = order["vat"];              // won't compile
-        // const int               badCast4 = order["vat"];              // compiles but throws exception
-        // const Json::list_type   badCast5 = order["items"];            // won't compile
-        // const Json::object_type badCast6 = order;                     // won't compile
-
         auto total = (1.0 + vat) * (firstItemCount * 10.99 + secondItemCount * 55.99);
 
         if (order["hasPromotion"])
@@ -386,7 +297,7 @@ TEST_CASE("Json")
 
         REQUIRE(!hasPreviousOrder);
 
-        REQUIRE(static_cast<std::string>(orderReceipt["orderId"]) == "471fc736-56e9-4a78-a256-4b6f641b7d13");
+        REQUIRE(static_cast<Json::string_type>(orderReceipt["orderId"]) == "471fc736-56e9-4a78-a256-4b6f641b7d13");
 
         REQUIRE(static_cast<double>(orderReceipt["total"]) == 180.2844);
     }
